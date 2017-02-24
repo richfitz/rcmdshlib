a71969e022c184f1369e469050c3b8a7fef8f7dd
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-02-24 10:09:37 +0000

    Last bit of testing

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -4,3 +4,8 @@ test_that("force colour", {
   res <- compiler_output_styles(TRUE)
   expect_is(res$error, "crayon")
 })
+
+test_that("shortcircuit operator", {
+  expect_equal(NULL %||% 1, 1)
+  expect_equal(0 %||% 1, 0)
+})
